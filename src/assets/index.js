e8ff79810f9b0841a444fe2951148114e7654b6f
Mathieu <mathieu@woorank.com>
2018-10-06 11:13:28 +0200

    Export assets

---
+++ src/assets/index.js
@@ -0,0 +1,47 @@
+import amoureux from './amoureux.jpg';
+import bateleur from './bateleur.jpg';
+import chariot from './chariot.jpg';
+import diable from './diable.jpg';
+import empereur from './empereur.jpg';
+import etoile from './etoile.jpg';
+import force from './force.jpg';
+import fortune from './fortune.jpg';
+import hermite from './hermite.jpg';
+import imperatrice from './imperatrice.jpg';
+import jugement from './jugement.jpg';
+import justice from './justice.jpg';
+import lune from './lune.jpg';
+import maison from './maison.jpg';
+import mat from './mat.jpg';
+import monde from './monde.jpg';
+import pape from './pape.jpg';
+import papesse from './papesse.jpg';
+import pendu from './pendu.jpg';
+import soleil from './soleil.jpg';
+import temperance from './temperance.jpg';
+import treize from './treize.jpg';
+
+export default {
+  amoureux,
+  bateleur,
+  chariot,
+  diable,
+  empereur,
+  etoile,
+  force,
+  fortune,
+  hermite,
+  imperatrice,
+  jugement,
+  justice,
+  lune,
+  maison,
+  mat,
+  monde,
+  pape,
+  papesse,
+  pendu,
+  soleil,
+  temperance,
+  treize
+};
