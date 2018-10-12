7f707ef890cf9e1254ef9a978cd12b6da2e5db24
Mathieu <mathieu@woorank.com>
2018-10-12 16:43:50 +0200

    Update cards data

--- src/cards.js
+++ src/cards.js
@@ -3,133 +3,199 @@ export const cards = [
     id: 'fool',
     name: 'The Fool',
     number: 0,
-    attributes: ['one', 'two']
+    element: 'Air',
+    astro: 'Uranus',
+    upright: ['god', 'beginnings', 'innocence', 'creation', 'free spirit'],
+    reversed: ['human', 'naivety', 'foolishness', 'recklessness', 'nature']
   },
   {
     id: 'magician',
     name: 'The Magician',
     number: 1,
-    attributes: ['one', 'two']
+    element: 'Air/Earth',
+    astro: 'Mercury',
+    upright: ['power', 'skill', 'concentration', 'action', 'resourcefulness'],
+    reversed: ['manipulation', 'idleness', 'poor planning', 'latent talents']
   },
   {
     id: 'priestess',
     name: 'The High Priestess',
     number: 2,
-    attributes: ['one', 'two']
+    element: 'Water',
+    astro: 'Moon',
+    upright: ['intuition', 'higher powers', 'mystery', 'subconscious', 'faith'],
+    reversed: ['secret', 'hidden agendas', 'detachment', 'need to listen to inner voice']
   },
   {
     id: 'empress',
     name: 'The Empress',
     number: 3,
-    attributes: ['one', 'two']
+    element: 'Earth',
+    astro: 'Venus',
+    upright: ['fertility', 'femininity', 'beauty', 'imagination', 'abundance'],
+    reversed: ['creative block', 'castration', 'dependence on others']
   },
   {
     id: 'emperor',
     name: 'The Emperor',
     number: 4,
-    attributes: ['one', 'two']
+    element: 'Fire',
+    astro: 'Mars',
+    upright: ['authority', 'father', 'structure', 'foundation'],
+    reversed: ['domination', 'excessive control', 'rigidity']
   },
   {
     id: 'hierophant',
     name: 'The Hierophant',
     number: 5,
-    attributes: ['one', 'two']
+    element: 'Earth',
+    astro: 'Taurus',
+    upright: ['religion', 'group', 'conformity', 'tradition', 'beliefs'],
+    reversed: ['restriction', 'abuse', 'peer pressure', 'challenging the status quo']
   },
   {
     id: 'lovers',
     name: 'The Lovers',
     number: 6,
-    attributes: ['one', 'two']
+    element: 'Air',
+    astro: 'Gemini',
+    upright: ['love', 'union', 'relationships', 'values alignment', 'choices'],
+    reversed: ['disharmony', 'imbalance', 'youth', 'indecisiveness']
   },
   {
     id: 'chariot',
     name: 'The Chariot',
     number: 7,
-    attributes: ['one', 'two']
+    element: 'Water',
+    astro: 'Cancer',
+    upright: ['control', 'willpower', 'victory', 'assertion', 'determination'],
+    reversed: ['lack of control and direction', 'arrogance', 'ruthlessness', 'aggression']
   },
   {
     id: 'justice',
     name: 'Justice',
     number: 8,
-    attributes: ['one', 'two']
+    element: 'Air',
+    astro: 'Leo',
+    upright: ['fairness', 'decision', 'truth', 'cause and effect', 'law'],
+    reversed: ['unfairness', 'lack of accountability', 'lie', 'dishonesty']
   },
   {
     id: 'hermit',
     name: 'The Hermit',
     number: 9,
-    attributes: ['one', 'two']
+    element: 'Earth',
+    astro: 'Virgo',
+    upright: ['soul-searching', 'introspection', 'learning', 'quest', 'inner guidance'],
+    reversed: ['isolation', 'loneliness', 'withdrawal', 'darkness', 'ignorance']
   },
   {
     id: 'fortune',
     name: 'Wheel of Fortune',
     number: 10,
-    attributes: ['one', 'two']
+    element: 'Fire/Water',
+    astro: 'Jupiter',
+    upright: ['good luck', 'cycles', 'fate', 'a turning point'],
+    reversed: ['bad luck', 'external forces', 'instability', 'out of control']
   },
   {
     id: 'strength',
     name: 'Strength',
     number: 11,
-    attributes: ['one', 'two']
+    element: 'Fire',
+    astro: 'Libra',
+    upright: ['courage', 'patience', 'control', 'compassion'],
+    reversed: ['weakness', 'self-doubt', 'violence', 'lack of discipline']
   },
   {
     id: 'hanged',
     name: 'The Hanged Man',
     number: 12,
-    attributes: ['one', 'two']
+    element: 'Water',
+    astro: 'Neptune',
+    upright: ['suspension', 'restriction', 'meditation', 'sacrifice'],
+    reversed: ['martyrdom', 'indecision', 'delay', 'pain']
   },
   {
     id: 'death',
     name: 'Death',
     number: 13,
-    attributes: ['one', 'two']
+    element: 'Water',
+    astro: 'Scorpio',
+    upright: ['endings', 'beginnings', 'change', 'transformation', 'transition'],
+    reversed: ['resistance to change', 'unable to move on', 'endless', 'perpetuity']
   },
   {
     id: 'temperance',
     name: 'Temperance',
     number: 14,
-    attributes: ['one', 'two']
+    element: 'Fire',
+    astro: 'Sagittarius',
+    upright: ['balance', 'moderation', 'patience', 'purpose', 'meaning'],
+    reversed: ['imbalance', 'excess', 'lack of long-term vision']
   },
   {
     id: 'devil',
     name: 'The Devil',
     number: 15,
-    attributes: ['one', 'two']
+    element: 'Earth',
+    astro: 'Capricorn',
+    upright: ['addiction', 'sexuality', 'materialism', 'submission'],
+    reversed: ['detachment', 'abstinence', 'breaking free', 'power reclaimed']
   },
   {
     id: 'tower',
     name: 'The Tower',
     number: 16,
-    attributes: ['one', 'two']
+    element: 'Fire/Water',
+    astro: 'Mars',
+    upright: ['disaster', 'upheaval', 'punishment', 'revelation'],
+    reversed: ['escape', 'flight', 'fear of change']
   },
   {
     id: 'star',
     name: 'The Star',
     number: 17,
-    attributes: ['one', 'two']
+    element: 'Air',
+    astro: 'Aquarius',
+    upright: ['hope', 'spirituality', 'renewal', 'inspiration', 'serenity'],
+    reversed: ['lack of faith', 'despair', 'discouragement', 'coldness']
   },
   {
     id: 'moon',
     name: 'The Moon',
     number: 18,
-    attributes: ['one', 'two']
+    element: 'Water',
+    astro: 'Pisces',
+    upright: ['illusion', 'fear', 'anxiety', 'insecurity', 'subconscious'],
+    reversed: ['release of fear', 'unhappiness', 'curiosity', 'secret', 'confusion']
   },
   {
     id: 'sun',
     name: 'The Sun',
     number: 19,
-    attributes: ['one', 'two']
+    element: 'Fire',
+    astro: 'Sun',
+    upright: ['fun', 'warmth', 'success', 'honesty', 'vitality'],
+    reversed: ['temporary depression', 'blindness', 'failure', 'burn out']
   },
   {
     id: 'judgement',
     name: 'Judgement',
     number: 20,
-    attributes: ['one', 'two']
+    element: 'Water/Fire',
+    astro: 'Pluto',
+    upright: ['rebirth', 'resolution', 'inner calling', 'absolution'],
+    reversed: ['doubt', 'absence of empathy', 'blind to own faults']
   },
   {
     id: 'world',
     name: 'The World',
     number: 21,
-    attributes: ['one', 'two']
+    element: 'Earth/Air',
+    astro: 'Saturn',
+    upright: ['completion', 'integration', 'travel', 'perfection'],
+    reversed: ['partial', 'imperfection', 'lack of closure', 'defect']
   }
 ];
 
