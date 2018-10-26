deeb42e5c2f67a3f6331e25a279340a5110bdb48
Mathieu <mathieu@woorank.com>
2018-10-26 17:22:20 +0200

    No classes in my neighbourhood

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Table from './Table';
 
-class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <Table />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className='App'>
+    <Table />
+  </div>
+);
 
 export default App;
