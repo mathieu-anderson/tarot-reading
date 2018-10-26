de4a9f45ec5bce0a52aa99aec25a70a4a616eb9d
Mathieu <mathieu@woorank.com>
2018-10-26 17:22:05 +0200

    Use function components instead of classes

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PureComponent } from 'react';
+import React, { memo, useState } from 'react';
 import { knuthShuffle as shuffle } from 'knuth-shuffle';
 
 import { cards } from './cards';
@@ -21,115 +21,99 @@ const getRandomNumber = (max) => shuffle(getRange(0, max).slice(0))[0];
 const getRandomCard = (deck) => getShuffledDeck(deck)[getRandomNumber(deck.length - 1)];
 const getRandomOrientation = () => shuffle(['upright', 'reversed'])[0];
 
-class Card extends PureComponent {
-  render () {
-    const { card, title } = this.props;
-    const orientation = getRandomOrientation();
+const Card = memo(props => {
+  const { card, title } = props;
+  const orientation = getRandomOrientation();
 
-    if (!card) {
-      return <div className='Table-card'>
-        <h3>
-          {title}
-        </h3>
-      </div>;
-    }
+  if (!card) {
     return <div className='Table-card'>
       <h3>
         {title}
       </h3>
-      {
-        orientation === 'upright'
-          ? assets[card.id]
-          : <div className='Table-card-reversed'>{assets[card.id]}</div>
-      }
-      <h3>
-        {card.number} - {card.name}
-      </h3>
-      <h4 />
-      <div className='Table-card-description'>
-        <div>
-          <b>Element</b>: {card.element}
-        </div>
-        <div>
-          <b>Astrology</b>: {card.astro}
-        </div>
-        {
-          orientation === 'upright'
-            ? <div>
-              <b>Key</b>: {card.upright[getRandomNumber(card.upright.length - 1)]}
-            </div>
-            : <div>
-              <b>Key</b>: {card.reversed[getRandomNumber(card.reversed.length - 1)]}
-            </div>
-        }
-      </div>
     </div>;
   }
-}
 
-export class Table extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      deck: cards,
-      cards: []
-    };
-    this.handleDraw = this.handleDraw.bind(this);
-    this.handleShuffle = this.handleShuffle.bind(this);
-  }
+  return <div className='Table-card'>
+    <h3>
+      {title}
+    </h3>
+    {
+      orientation === 'upright'
+        ? assets[card.id]
+        : <div className='Table-card-reversed'>{assets[card.id]}</div>
+    }
+    <h3>
+      {card.number} - {card.name}
+    </h3>
+    <h4 />
+    <div className='Table-card-description'>
+      <div>
+        <b>Element</b>: {card.element}
+      </div>
+      <div>
+        <b>Astrology</b>: {card.astro}
+      </div>
+      {
+        orientation === 'upright'
+          ? <div>
+            <b>Key</b>: {card.upright[getRandomNumber(card.upright.length - 1)]}
+          </div>
+          : <div>
+            <b>Key</b>: {card.reversed[getRandomNumber(card.reversed.length - 1)]}
+          </div>
+      }
+    </div>
+  </div>;
+});
 
-  handleDraw () {
-    if (this.state.deck.length <= 0) {
-      return window.alert('The deck is empty, please shuffle.');
+const Table = () => {
+  const [deck, setDeck] = useState(cards);
+  const [drawnCards, setDrawnCards] = useState([]);
+
+  const handleShuffle = () => {
+    setDeck(getShuffledDeck(cards));
+    setDrawnCards([]);
+  };
+
+  const handleDraw = () => {
+    if (deck.length <= 0) {
+      return;
     }
 
-    const card = getRandomCard(this.state.deck);
-    const nextDeck = removeFromDeck(this.state.deck, card);
+    const drawnCard = getRandomCard(deck);
+    const nextDeck = removeFromDeck(deck, drawnCard);
 
-    this.setState({
-      deck: nextDeck,
-      cards: [...this.state.cards, card]
-    });
-  }
+    setDeck(nextDeck);
+    setDrawnCards([...drawnCards, drawnCard]);
+  };
 
-  handleShuffle () {
-    this.setState({
-      deck: getShuffledDeck(cards),
-      cards: []
-    });
-  }
-
-  render () {
-    const drawnCards = this.state.cards;
-
-    return (
-      <React.Fragment>
-        <div className='Table-container'>
-          <div className='Table-buttons'>
-            <div className='Table-reset' onClick={this.handleShuffle}>
-              Shuffle deck
-            </div>
-            {
-              drawnCards.length < 4
-                ? <div className='Table-draw' onClick={this.handleDraw}>
-                  Draw a card
-                </div>
-                : <div className='Table-draw inactive'>
-                  Draw a card
-                </div>
-            }
-
+  return (
+    <React.Fragment>
+      <div className='Table-container'>
+        <div className='Table-buttons'>
+          <div className='Table-reset' onClick={handleShuffle}>
+          Shuffle deck
           </div>
+          {
+            drawnCards.length < 4
+              ? <div className='Table-draw' onClick={handleDraw}>
+              Draw a card
+              </div>
+              : <div className='Table-draw inactive'>
+              Draw a card
+              </div>
+          }
+
         </div>
-        <div className='Table-cards'>
-          <Card card={drawnCards[0]} title='Inside' />
-          <Card card={drawnCards[1]} title='Outside' />
-          <Card card={drawnCards[2]} title='Communication' />
-          <Card card={drawnCards[3]} title='Outcome' />
-        </div>
-      </React.Fragment>
-    );
-  }
-}
+      </div>
+      <div className='Table-cards'>
+        <Card card={drawnCards[0]} title='Inside' />
+        <Card card={drawnCards[1]} title='Outside' />
+        <Card card={drawnCards[2]} title='Communication' />
+        <Card card={drawnCards[3]} title='Outcome' />
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Table;
